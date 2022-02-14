c89f5c0d5aae5a43e0d18d4b541c95ded85ab860
Lauro Otavio Urbano <74082012+laurourbano@users.noreply.github.com>
2022-02-14 09:20:46 -0300

    upload

---
+++ main.js
@@ -0,0 +1,167 @@
+//const texto = document.querySelectorAll('.texto');
+//const span = document.querySelectorAll('.span');
+//const botao = document.querySelectorAll('.botao');
+
+function copiaTexto(){
+    const texto = document.querySelectorAll('.texto');
+    const span = document.querySelectorAll('.span');
+    const botao = document.querySelectorAll('.botao');
+    let tudo = span[arguments(0)];
+    document.write(tudo);
+    };
+
+copiaTexto();
+/*var textarea    = document.getElementById("texto");
+    var answer      = document.getElementById("copyAnswer");
+    var copy        = document.getElementById("copyBlock");
+
+    var textarea1    = document.getElementById("texto1");
+    var answer1      = document.getElementById("copyAnswer1");
+    var copy1        = document.getElementById("copyBlock1");
+
+    var textarea2    = document.getElementById("texto2");
+    var answer2      = document.getElementById("copyAnswer2");
+    var copy2        = document.getElementById("copyBlock2");
+
+    var textarea3    = document.getElementById("texto3");
+    var answer3      = document.getElementById("copyAnswer3");
+    var copy3        = document.getElementById("copyBlock3");
+
+    var textarea4    = document.getElementById("texto4");
+    var answer4      = document.getElementById("copyAnswer4");
+    var copy4        = document.getElementById("copyBlock4");
+
+    var textarea5    = document.getElementById("texto5");
+    var answer5      = document.getElementById("copyAnswer5");
+    var copy5        = document.getElementById("copyBlock5");
+
+    var textarea6    = document.getElementById("texto6");
+    var answer6      = document.getElementById("copyAnswer6");
+    var copy6        = document.getElementById("copyBlock6");
+
+    var textarea7    = document.getElementById("texto7");
+    var answer7      = document.getElementById("copyAnswer7");
+    var copy7        = document.getElementById("copyBlock7");
+    
+    var textarea8    = document.getElementById("texto8");
+    var answer8      = document.getElementById("copyAnswer8");
+    var copy8        = document.getElementById("copyBlock8");
+
+    var textarea9    = document.getElementById("texto9");
+    var answer9      = document.getElementById("copyAnswer9");
+    var copy9        = document.getElementById("copyBlock9");
+
+    var textarea10    = document.getElementById("texto10");
+    var answer10      = document.getElementById("copyAnswer10");
+    var copy10        = document.getElementById("copyBlock10");*/
+
+/*copy.addEventListener('click', function(e) {
+        textarea.select();
+            try {
+                var successful = document.execCommand('copy');
+                if(successful) answer.innerHTML = 'Copiado!';
+                else answer.innerHTML = 'Não foi possível copiar!';
+            } catch (err) {
+                answer.innerHTML = 'Navegador sem suporte!';
+            }
+    });
+    copy1.addEventListener('click', function(e) {
+        textarea1.select();
+            try {
+                var successful1 = document.execCommand('copy');
+                if(successful1) answer1.innerHTML = 'Copiado!';
+                else answer1.innerHTML = 'Não foi possível copiar!';
+            } catch (err) {
+                answer1.innerHTML = 'Navegador sem suporte!';
+            }
+    });
+    copy2.addEventListener('click', function(e) {
+        textarea2.select();
+            try {
+                var successful2 = document.execCommand('copy');
+                if(successful2) answer2.innerHTML = 'Copiado!';
+                else answer2.innerHTML = 'Não foi possível copiar!';
+            } catch (err) {
+                answer2.innerHTML = 'Navegador sem suporte!';
+            }
+    });
+    copy3.addEventListener('click', function(e) {
+        textarea3.select();
+            try {
+                var successful3 = document.execCommand('copy');
+                if(successful3) answer3.innerHTML = 'Copiado!';
+                else answer3.innerHTML = 'Não foi possível copiar!';
+            } catch (err) {
+                answer3.innerHTML = 'Navegador sem suporte!';
+            }
+    });
+    copy4.addEventListener('click', function(e) {
+        textarea4.select();
+            try {
+                var successful4 = document.execCommand('copy');
+                if(successful4) answer4.innerHTML = 'Copiado!';
+                else answer4.innerHTML = 'Não foi possível copiar!';
+            } catch (err) {
+                answer4.innerHTML = 'Navegador sem suporte!';
+            }
+    });
+    copy5.addEventListener('click', function(e) {
+        textarea5.select();
+            try {
+                var successful5 = document.execCommand('copy');
+                if(successful5) answer5.innerHTML = 'Copiado!';
+                else answer5.innerHTML = 'Não foi possível copiar!';
+            } catch (err) {
+                answer5.innerHTML = 'Navegador sem suporte!';
+            }
+    });
+    copy6.addEventListener('click', function(e) {
+        textarea6.select();
+            try {
+                var successful6 = document.execCommand('copy');
+                if(successful6) answer6.innerHTML = 'Copiado!';
+                else answer6.innerHTML = 'Não foi possível copiar!';
+            } catch (err) {
+                answer6.innerHTML = 'Navegador sem suporte!';
+            }
+    });
+    copy7.addEventListener('click', function(e) {
+        textarea7.select();
+            try {
+                var successful7 = document.execCommand('copy');
+                if(successful7) answer7.innerHTML = 'Copiado!';
+                else answer7.innerHTML = 'Não foi possível copiar!';
+            } catch (err) {
+                answer7.innerHTML = 'Navegador sem suporte!';
+            }
+    });
+    copy8.addEventListener('click', function(e) {
+        textarea8.select();
+            try {
+                var successful8 = document.execCommand('copy');
+                if(successful8) answer8.innerHTML = 'Copiado!';
+                else answer8.innerHTML = 'Não foi possível copiar!';
+            } catch (err) {
+                answer8.innerHTML = 'Navegador sem suporte!';
+            }
+    });
+    copy9.addEventListener('click', function(e) {
+        textarea9.select();
+            try {
+                var successful9 = document.execCommand('copy');
+                if(successful9) answer9.innerHTML = 'Copiado!';
+                else answer9.innerHTML = 'Não foi possível copiar!';
+            } catch (err) {
+                answer9.innerHTML = 'Navegador sem suporte!';
+            }
+    });
+    copy10.addEventListener('click', function(e) {
+        textarea10.select();
+            try {
+                var successful10 = document.execCommand('copy');
+                if(successful10) answer10.innerHTML = 'Copiado!';
+                else answer10.innerHTML = 'Não foi possível copiar!';
+            } catch (err) {
+                answer10.innerHTML = 'Navegador sem suporte!';
+            }
+    });*/
