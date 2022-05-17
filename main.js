aeefd6b923d802ecc3b34b277db205771ee115aa
laurourbano <74082012+laurourbano@users.noreply.github.com>
2022-05-16 22:02:41 -0300

    update

--- main.js
+++ main.js
@@ -4,27 +4,27 @@ function changeMode() {
 }
 
 function changeClasses() {
-	button.classList.toggle(darkModeClass);
-	h1.classList.toggle(darkModeClass);
-	body.classList.toggle(darkModeClass);
-	footer.classList.toggle(darkModeClass);
+	button.classList.toggle(lightModeClass);
+	h1.classList.toggle(lightModeClass);
+	body.classList.toggle(lightModeClass);
+	footer.classList.toggle(lightModeClass);
 }
 
 function changeText() {
 	const lightMode = 'Light Mode';
 	const darkMode = 'Dark Mode';
 
-	if (body.classList.contains(darkModeClass)) {
+	if (body.classList.contains(lightModeClass)) {
 		button.innerHTML = lightMode;
-		h1.innerHTML = darkMode + ' ON';
+		h1.innerHTML = lightMode + ' ON';
 		return;
 	}
 
 	button.innerHTML = darkMode;
-	h1.innerHTML = lightMode + ' ON';
+	h1.innerHTML = darkMode + ' ON';
 }
 
-const darkModeClass = 'dark-mode';
+const lightModeClass = 'light-mode';
 const button = document.getElementById('mode-selector');
 const h1 = document.getElementById('page-title');
 const body = document.getElementsByTagName('body')[0];
