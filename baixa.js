a8780baeba0bc9705963968c4cf40ce4f7b4fb54
laurourbano <74082012+laurourbano@users.noreply.github.com>
2022-05-30 20:49:34 -0300

    1

--- baixa.js
+++ baixa.js
@@ -10,9 +10,3 @@ window.onload = function() {
     });
   });
 }
-
-var data = document.querySelector(".data");
-var dia = new Date().getDate();
-var mes = new Date().getMonth() + 1;
-var dataAtual = "_"+mes+"_"+dia;
-data.innerHTML += dataAtual;
