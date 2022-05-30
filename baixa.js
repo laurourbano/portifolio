3150171bb36242a1a3890ca07475ae1d7b8add70
Lauro Otavio Urbano <74082012+laurourbano@users.noreply.github.com>
2022-05-30 11:21:43 -0300

    update

--- baixa.js
+++ baixa.js
@@ -10,3 +10,9 @@ window.onload = function() {
     });
   });
 }
+
+var data = document.querySelector(".data");
+var dia = new Date().getDate();
+var mes = new Date().getMonth() + 1;
+var dataAtual = "_"+mes+"_"+dia;
+data.innerHTML += dataAtual;
