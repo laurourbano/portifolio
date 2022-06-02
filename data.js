7ec1c48097d2c658e32bf18c99a0e1028a76cf22
Lauro Otavio Urbano <74082012+laurourbano@users.noreply.github.com>
2022-06-02 10:45:51 -0300

    Update data.js

--- data.js
+++ data.js
@@ -2,19 +2,22 @@
 const data = document.querySelector(".data");
 const dia = new Date().getDate();
 const mes = new Date().getMonth() + 1;
-const dataAtual = "_"+mes+"_"+dia;
+const ano = new Date().getFullYear();
+const dataAtual = ano+"_"+mes+"_"+dia;
 data.innerHTML += dataAtual;
 
 const dataIndeferimento = document.querySelector(".dataIndeferimento");
 const dia1 = new Date().getDate();
 const mes1 = new Date().getMonth() + 1;
 const ano1 = new Date().getFullYear();
-const dataAtual1 = (dia1)+"/"+mes1+"/"+ano1;
+const dataAtual1 = dia1+"/"+mes1+"/"+ano1;
 dataIndeferimento.innerHTML = (`INDEFERIDO REQUERIMENTO DE BAIXA.
 DOCUMENTOS SOLICITADOS PARA CORREÇÃO DO PROCEDIMENTO DE BAIXA DE RT NÃO FORAM ENVIADOS NO PRAZO DE 01 
 (UM) DIA ÚTIL DA RESPOSTA DO CRF-PR.
 * DEVERÁ INICIAR NOVO REQUERIMENTO DE BAIXA DE RESPONSABILIDADE TÉCNICA, ANEXANDO TODOS OS DOCUMENTOS NECESSÁRIOS.
 FAVOR CONSULTAR PASSO A PASSO NO LINK https://www.crf-pr.org.br/noticia/visualizar/46459
+
+DATA DA SOLICITAÇÃO – ${dataAtual1}
 
 MOTIVO DO INDEFERIMENTO:
 – FALTOU QUEBRA DE VÍNCULO DE TRABALHO; (PÁGINA DA CTPS DO CONTRATO COM DATA DE SAÍDA E ASSINATURA DA 
@@ -27,6 +30,4 @@ OU
 - CASO AINDA NÃO POSSUA TODOS OS DOCUMENTOS, PODERÁ APRESENTAR  FORMULÁRIO DE REQUERIMENTO PREENCHIDO 
 E ASSINADO *PELO REPRESENTANTE LEGAL E PELO PROFISSIONAL*
 (MARCANDO TODOS OS DOCUMENTOS QUE FALTAM E O MOTIVO).
-- FALTA ASSINATURA, DO REPRESENTANTE LEGAL, NO REQUERIMENTO DE BAIXA PARA BAIXAR SEM DOCUMENTOS;
-
-DATA DA SOLICITAÇÃO – ${dataAtual1}`);
+- FALTA ASSINATURA, DO REPRESENTANTE LEGAL, NO REQUERIMENTO DE BAIXA PARA BAIXAR SEM DOCUMENTOS;`);
