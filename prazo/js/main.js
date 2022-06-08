6061dbfd8699edc6032a52f1957e1d4bb91f2268
Lauro Otavio Urbano <74082012+laurourbano@users.noreply.github.com>
2022-06-08 17:18:20 -0300

    Update main.js

--- prazo/js/main.js
+++ prazo/js/main.js
@@ -2,8 +2,11 @@ const prazo = [30, 60, 90, 120, 160];
 var data = document.querySelector('.data');
 var numero = data.value;
 var dataFim = document.querySelector('.dataFim');
-var final = numero + prazo(i);
-dataFim.innerHTML = final;
+for(i>5;i=0;i++){
+    var final = numero + prazo;
+    prazo++
+    dataFim.innerHTML = final;
+}
 console.log(numero);
 console.log(prazo);
 console.log(final);
