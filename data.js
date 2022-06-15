e5b7fcf655259e7980e977554d3e1213df75a8d1
laurourbano <74082012+laurourbano@users.noreply.github.com>
2022-06-14 22:35:30 -0300

    Update data.js

--- data.js
+++ data.js
@@ -46,6 +46,6 @@ var input = document.querySelector("#data");
 var prazo = document.querySelector(".prazo");
 var trinta = new Date();
 trinta.setDate(trinta.getDate() + 30); // Adiciona 3 dias
-input.addEventListner("input", prazo.value){
+input.addEventListener("input", prazo.value){
     return prazo.innerHTML = prazo.value;
 };
