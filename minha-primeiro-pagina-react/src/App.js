b4d2a2b9a2163ec16b4f3aae404bf3318feb6375
laurourbano <74082012+laurourbano@users.noreply.github.com>
2022-07-26 04:42:35 -0300

    pagina feita

--- minha-primeiro-pagina-react/src/App.js
+++ minha-primeiro-pagina-react/src/App.js
@@ -1,23 +1,40 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container">
+      <header>
+        <h1>React JS</h1>
+        <p>Biblioteca JavaScript para criar SPAs</p>
+
+        <a href='https://reactjs.org/' target="_blank" rel="noreferrer">Site Oficial</a>
       </header>
+
+      <section>
+        <div className="conteudo">
+          <h2>Requisitos</h2>
+          <p>Veja abaixo os requisitos mínimos para aprender React JS:</p>
+        </div>
+        <div className="cards">
+          <div className="Card">
+            <h3>HTML</h3>
+            <p>Aprender a estruturar os elementos da página com HTML</p>
+          </div>
+          <div className="Card">
+            <h3>CSS</h3>
+            <p>Aprender a estilizar os elementos da página com CSS</p>
+          </div>
+          <div className="Card">
+            <h3>JavaScript</h3>
+            <p>Aprender a linguagem utilizada pelo React JS, o JavaScript</p>
+          </div>  
+        </div>
+
+      </section>
+
+      <footer>
+        <p>Curso de introdução de React JS</p>
+      </footer>
     </div>
   );
 }
