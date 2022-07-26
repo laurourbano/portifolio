42e9cd6f30d56fa93f8bfb5dd3614e942900becb
laurourbano <74082012+laurourbano@users.noreply.github.com>
2022-07-26 18:34:55 -0300

    novo projeto

---
+++ project2/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './Componentes/Header/'
+import Main from './Componentes/Main/'
+import Footer from './Componentes/Footer/'
+
+function App() {
+  return (
+    <div className="container">
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+    );
+}
+
+export default App;
---
+++ project2/src/Componentes/Main/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Main() {
+  return (
+    <div>
+      <main>
+        <h1>HomePage</h1>
+      </main>
+    </div>
+  )
+};
